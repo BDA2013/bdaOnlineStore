9ce27ac2d324f3d2c4acc439216cd14ebcb36afa
BDA2013 <129307670+BDA2013@users.noreply.github.com>
2023-08-09 15:12:15 -0400

    connected to mysql

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,20 +5,17 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-Product.belongsTo(Category, {
-  foreignKey: 'category_id' 
-})
+Product.belongsTo(Category)
+
 // Categories have many Products
-Category.hasMany(Product, {
-  foreignKey: 'product_id'
-})
+Category.hasMany(Product)
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
-  foreignKey: 'product_tag_id'
+  through: ProductTag
 })
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
-  foreignKey: 'product_tag_id'
+  through: ProductTag
 })
 
 module.exports = {
